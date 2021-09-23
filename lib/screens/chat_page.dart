adb1567666f001b29a7e61849bb5cd888e2f2592
Raimbek <rrakhimbekov@gmail.com>
2021-09-23 23:00:59 +0600

    ConversationList widget

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yam/widgets/conversation_list.dart';
+
+import 'models/chat_users_model.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -6,6 +9,54 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatUsers> chatUsers = [
+    ChatUsers(
+        name: "Jane Russel",
+        messageText: "Awesome Setup",
+        imageURL: "https://randomuser.me/api/portraits/women/1.jpg",
+        time: "Now"),
+    ChatUsers(
+        name: "Glady's Murphy",
+        messageText: "That's Great",
+        imageURL: "https://randomuser.me/api/portraits/women/2.jpg",
+        time: "Yesterday"),
+    ChatUsers(
+        name: "Jorge Henry",
+        messageText: "Hey where are you?",
+        imageURL: "https://randomuser.me/api/portraits/men/3.jpg",
+        time: "31 Mar"),
+    ChatUsers(
+        name: "Philip Fox",
+        messageText: "Busy! Call me in 20 mins",
+        imageURL: "https://randomuser.me/api/portraits/men/4.jpg",
+        time: "28 Mar"),
+    ChatUsers(
+        name: "Debra Hawkins",
+        messageText: "Thankyou, It's awesome",
+        imageURL: "https://randomuser.me/api/portraits/women/5.jpg",
+        time: "23 Mar"),
+    ChatUsers(
+        name: "Jacob Pena",
+        messageText: "will update you in evening",
+        imageURL: "<https://randomuser.me/api/portraits/men/6.jpg>",
+        time: "17 Mar"),
+    ChatUsers(
+        name: "Andrey Jones",
+        messageText: "Can you please share the file?",
+        imageURL: "https://randomuser.me/api/portraits/men/7.jpg",
+        time: "24 Feb"),
+    ChatUsers(
+        name: "John Wick",
+        messageText: "How are you?",
+        imageURL: "https://randomuser.me/api/portraits/men/8.jpg",
+        time: "18 Feb"),
+    ChatUsers(
+        name: "R R",
+        messageText: "How are you?",
+        imageURL: "https://randomuser.me/api/portraits/men/9.jpg",
+        time: "18 Feb"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,6 +125,21 @@ class _ChatPageState extends State<ChatPage> {
                       borderSide: BorderSide(color: Colors.grey.shade100)),
                 ),
               ),
+            ),
+            ListView.builder(
+              itemCount: chatUsers.length,
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 16),
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return ConversationList(
+                  name: chatUsers[index].name,
+                  messageText: chatUsers[index].messageText,
+                  imageUrl: chatUsers[index].imageURL,
+                  time: chatUsers[index].time,
+                  isMessageRead: (index == 0 || index == 3) ? true : false,
+                );
+              },
             ),
           ],
         ),
