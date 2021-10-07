e8ae24d207d561e0da5d88a9d8881e7dfa39a6ab
Raimbek <rrakhimbekov@gmail.com>
2021-10-08 03:31:01 +0600

    refactor

--- lib/widgets/chat_page.dart
+++ lib/widgets/chat_page.dart
@@ -12,57 +12,11 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  List<ChatUsers> chatUsers = [
-    ChatUsers(
-        name: "Jane Russel",
-        messageText: "Awesome Setup",
-        imageURL: "https://randomuser.me/api/portraits/women/1.jpg",
-        time: "Now"),
-    ChatUsers(
-        name: "Glady's Murphy",
-        messageText: "That's Great",
-        imageURL: "https://randomuser.me/api/portraits/women/2.jpg",
-        time: "Yesterday"),
-    ChatUsers(
-        name: "Jorge Henry",
-        messageText: "Hey where are you?",
-        imageURL: "https://randomuser.me/api/portraits/men/3.jpg",
-        time: "31 Mar"),
-    ChatUsers(
-        name: "Philip Fox",
-        messageText: "Busy! Call me in 20 mins",
-        imageURL: "https://randomuser.me/api/portraits/men/4.jpg",
-        time: "28 Mar"),
-    ChatUsers(
-        name: "Debra Hawkins",
-        messageText: "Thankyou, It's awesome",
-        imageURL: "https://randomuser.me/api/portraits/women/5.jpg",
-        time: "23 Mar"),
-    ChatUsers(
-        name: "Jacob Pena",
-        messageText: "will update you in evening",
-        imageURL: "https://randomuser.me/api/portraits/men/6.jpg",
-        time: "17 Mar"),
-    ChatUsers(
-        name: "Andrey Jones",
-        messageText: "Can you please share the file?",
-        imageURL: "https://randomuser.me/api/portraits/men/7.jpg",
-        time: "24 Feb"),
-    ChatUsers(
-        name: "John Wick",
-        messageText: "How are you?",
-        imageURL: "https://randomuser.me/api/portraits/men/8.jpg",
-        time: "18 Feb"),
-    ChatUsers(
-        name: "R R",
-        messageText: "How are you?",
-        imageURL: "https://randomuser.me/api/portraits/men/9.jpg",
-        time: "18 Feb"),
-  ];
+  List<ChatUsers> chatUsers = [];
 
   void fetchUserList() async {
-    final response = await http
-        .get(Uri.parse('http://192.168.43.144:8080/api/user/list'));
+    final response =
+        await http.get(Uri.parse('http://192.168.43.144:8080/api/user/list'));
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
@@ -73,14 +27,79 @@ class _ChatPageState extends State<ChatPage> {
       print(json);
 
       var userList = json['userInfos'] as List;
-      userList.forEach((element) {
-      });
-
+      userList.forEach((element) {});
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
       //throw Exception('Failed to load album');
     }
+  }
+  
+  @override
+  void initState() {
+    chatUsers.addAll([
+      ChatUsers(
+          name: "Jane Russel",
+          messageText: "Awesome Setup",
+          imageURL: "https://randomuser.me/api/portraits/women/1.jpg",
+          time: "Now"),
+      ChatUsers(
+          name: "Glady's Murphy",
+          messageText: "That's Great",
+          imageURL: "https://randomuser.me/api/portraits/women/2.jpg",
+          time: "Yesterday"),
+      ChatUsers(
+          name: "Jorge Henry",
+          messageText: "Hey where are you?",
+          imageURL: "https://randomuser.me/api/portraits/men/3.jpg",
+          time: "31 Mar"),
+      ChatUsers(
+          name: "Philip Fox",
+          messageText: "Busy! Call me in 20 mins",
+          imageURL: "https://randomuser.me/api/portraits/men/4.jpg",
+          time: "28 Mar"),
+      ChatUsers(
+          name: "Debra Hawkins",
+          messageText: "Thankyou, It's awesome",
+          imageURL: "https://randomuser.me/api/portraits/women/5.jpg",
+          time: "23 Mar"),
+      ChatUsers(
+          name: "Jacob Pena",
+          messageText: "will update you in evening",
+          imageURL: "https://randomuser.me/api/portraits/men/6.jpg",
+          time: "17 Mar"),
+      ChatUsers(
+          name: "Andrey Jones",
+          messageText: "Can you please share the file?",
+          imageURL: "https://randomuser.me/api/portraits/men/7.jpg",
+          time: "24 Feb"),
+      ChatUsers(
+          name: "John Wick",
+          messageText: "How are you?",
+          imageURL: "https://randomuser.me/api/portraits/men/8.jpg",
+          time: "18 Feb"),
+      ChatUsers(
+          name: "R R",
+          messageText: "How are you?",
+          imageURL: "https://randomuser.me/api/portraits/men/9.jpg",
+          time: "18 Feb"),
+      ChatUsers(
+          name: "R R",
+          messageText: "How are you?",
+          imageURL: "https://randomuser.me/api/portraits/men/9.jpg",
+          time: "18 Feb"),
+      ChatUsers(
+          name: "Qee R",
+          messageText: "How are you?",
+          imageURL: "https://randomuser.me/api/portraits/men/9.jpg",
+          time: "18 Feb"),
+      ChatUsers(
+          name: "Seee R",
+          messageText: "How are you?",
+          imageURL: "https://randomuser.me/api/portraits/men/9.jpg",
+          time: "18 Feb"),
+    ]);
+    super.initState();
   }
 
   @override
@@ -91,66 +110,8 @@ class _ChatPageState extends State<ChatPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SafeArea(
-              child: Padding(
-                padding: EdgeInsets.only(left: 16, right: 16, top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Conversations",
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
-                      height: 30,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.pink[50],
-                      ),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.add,
-                            color: Colors.pink,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 2,
-                          ),
-                          Text(
-                            "Add New",
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "...",
-                  hintStyle: TextStyle(color: Colors.grey.shade600),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.grey.shade600,
-                    size: 20,
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey.shade100,
-                  contentPadding: EdgeInsets.all(8),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: Colors.grey.shade100)),
-                ),
-              ),
+            SizedBox(
+              height: 30,
             ),
             ListView.builder(
               itemCount: chatUsers.length,
