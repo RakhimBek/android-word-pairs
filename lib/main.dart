09863b1421e1e6115edc6db45301a7e1d021c36c
Raimbek <rrakhimbekov@gmail.com>
2021-10-15 00:44:42 +0600

    LoginForm

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:yam/widgets/home_page.dart';
+import 'package:yam/widgets/login_form.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginForm(),
     );
   }
 }
---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:yam/widgets/home_page.dart';
+
+class LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: CupertinoColors.activeGreen,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: CupertinoColors.lightBackgroundGray,
+        body: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  decoration: const BoxDecoration(
+                    color: CupertinoColors.systemGrey6,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const TextField(
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      hintText: "Login...",
+                      hintStyle: TextStyle(color: Colors.black54),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  decoration: const BoxDecoration(
+                    color: CupertinoColors.systemGrey6,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: const TextField(
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      hintText: "Password...",
+                      hintStyle: TextStyle(color: Colors.black54),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  decoration: const BoxDecoration(
+                    color: CupertinoColors.systemIndigo,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return HomePage();
+                          },
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "LOG IN",
+                      style: TextStyle(
+                        color: CupertinoColors.white,
+                        //backgroundColor: CupertinoColors.inactiveGray,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
