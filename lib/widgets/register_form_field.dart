04eb90c8664496c0f04b25ea40f72549cb4c99c2
Raimbek <rrakhimbekov@gmail.com>
2021-10-24 22:15:32 +0600

    RegisterFormField: visibility param

--- lib/widgets/register_form_field.dart
+++ lib/widgets/register_form_field.dart
@@ -5,33 +5,43 @@ class RegisterFormField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final bool obscureText;
+  final bool visibility;
 
-  const RegisterFormField(
-      {Key? key,
-      required this.controller,
-      required this.hintText,
-      required this.obscureText})
-      : super(key: key);
+  const RegisterFormField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+    this.visibility = true,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(left: 10, right: 10),
-      margin: const EdgeInsets.only(left: 20, right: 20),
-      decoration: const BoxDecoration(
-        color: CupertinoColors.systemGrey6,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText,
-        enableSuggestions: false,
-        autocorrect: false,
-        textAlign: TextAlign.left,
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: const TextStyle(color: Colors.black54),
-          border: InputBorder.none,
+    return Visibility(
+      maintainSize: true,
+      maintainAnimation: true,
+      maintainState: true,
+      maintainInteractivity: false,
+      maintainSemantics: false,
+      visible: true,
+      child: Container(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        margin: const EdgeInsets.only(left: 20, right: 20),
+        decoration: const BoxDecoration(
+          color: CupertinoColors.systemGrey6,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: TextField(
+          controller: controller,
+          obscureText: obscureText,
+          enableSuggestions: false,
+          autocorrect: false,
+          textAlign: TextAlign.left,
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(color: Colors.black54),
+            border: InputBorder.none,
+          ),
         ),
       ),
     );
